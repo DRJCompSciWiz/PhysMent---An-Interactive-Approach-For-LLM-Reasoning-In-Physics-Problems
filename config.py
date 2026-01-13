febf00f92d753fa3d5f4e2001ff46e4a38bbd60f
Eddie Hu <eddiehu0314@gmail.com>
2026-01-14 01:13:39 +0800

    add requirements.txt

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 iterations: list[int] = [
     5,
 ]  # iterations LLM gets before hard stop
-start_scene: int = 151  # first test scene
+start_scene: int = 153  # first test scene
 end_scene: int = 158  # last test scene
 
 # NOTE: Agent management
